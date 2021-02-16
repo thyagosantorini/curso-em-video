ee0bd6ed4bdc7dd81a11ca65c989f3da14f5bf3e
Thyago Santorini <thyago@thyagosantorini.com>
2021-02-15 21:30:17 -0300

    Create ambiente.js

---
+++ javascript/aula13/ambiente.js
@@ -0,0 +1,24 @@
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador++
+} while (contador <= 8)
+
+
+/*
+var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
