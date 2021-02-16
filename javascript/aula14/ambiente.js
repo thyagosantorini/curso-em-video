c4c0ffcfeda2e071404299b295a32cf709e120fd
Thyago Santorini <thyago@thyagosantorini.com>
2021-02-15 22:30:24 -0300

    create ex016

---
+++ javascript/aula14/ambiente.js
@@ -0,0 +1,15 @@
+/*
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+do {
+    console.log(c)
+    c++
+} while(c <= 5)
+*/
+// -----------------------
+for (var c = 1;c <= 3;c++){
+    console.log(c)
+}
