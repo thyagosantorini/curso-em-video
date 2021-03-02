2f3d7400fa1d32850622b11326df6f594e49f268
Thyago Santorini <thyago@thyagosantorini.com>
2021-03-02 18:34:52 -0300

    Upload aula16 JavaScript
    Upload da aula 16 de JavaScript do Curso em Vídeo

---
+++ javascript/aula16/funcao04.js
@@ -0,0 +1,11 @@
+function fatorial(n) {
+    let fator = 1
+    for(let c = n; c > 1; c--) {
+        fator *= c
+    }
+    return fator
+}
+
+console.log(fatorial(5))
+
+// 5! = 5 X 4 X 3 X 2 X 1 = 120
